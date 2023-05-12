10abe625305e7d41704f73335d240182df93623c
Jagdish Vivekanand Kadam <124343964+jayvkadam17@users.noreply.github.com>
2023-05-12 22:07:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// click event
+//  dbclick event
 
-// An element receives a click event when a pointing device button (such as a mouse's primary mouse button)
-//  is both pressed and released while the pointer is located inside the element.
-
-// If the button is pressed on one element and the pointer is moved outside the element before the button is released,
-//  the event is fired on the most specific ancestor element that contained both elements.
+// The dblclick event fires when a pointing device button (such as a mouse's primary button)
+//  is double-clicked; that is, when it's rapidly clicked twice on a single element within a very short span of time.
 
 const boxElement = document.getElementById("box");
 
-// add a click event listener to the element
-boxElement.addEventListener("click", function () {
-  alert("you just clicked me 👍");
+// adding a dblclick event listener to the element
+boxElement.addEventListener("dblclick", function () {
+  alert("doubleClick event is working properly 👍");
 });
